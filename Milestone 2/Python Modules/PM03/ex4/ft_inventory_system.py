2ccfdd19f1dec691154bb224ccb1e5b1a4f3ae13
Naki Neves <joshie.cad@gmail.com>
2026-02-25 18:17:58 +0000

    finished ex4 and did most of ex5 of PM03

--- Milestone 2/Python Modules/PM03/ex4/ft_inventory_system.py
+++ Milestone 2/Python Modules/PM03/ex4/ft_inventory_system.py
@@ -6,7 +6,7 @@
 #  By: joshde-s <joshde-s@student.42porto.com>   +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/02/23 19:24:14 by joshde-s        #+#    #+#               #
-#  Updated: 2026/02/24 18:49:51 by joshde-s        ###   ########.fr        #
+#  Updated: 2026/02/25 13:17:03 by joshde-s        ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 import sys
@@ -34,7 +34,6 @@ def main() -> None:
             inventory[key] = int(value)
         except ValueError:
             print(f"Ignoring invalid argument: {arg}")
-    keys: list[str] = inventory.keys()
     values: list[int] = inventory.values()
     values_total: int = 0
     for value in values:
@@ -43,21 +42,133 @@ def main() -> None:
     total_keys: int = len(inventory)
     print(f"Unique item types: {total_keys}")
 
-    nested_invt = {value: {key: value} for (key, value) in inventory.items()}
-    print(nested_invt)
-
-
     print("\n=== Current Inventory ===")
+
+    sorted: dict[dict: int[str, int]] = {}
+    biggest: int = 0
+    smallest: int = values_total
     for key in inventory:
-        percentage = "%.1f" % ((inventory[key] / values_total) * 100)
-        print(f"{key}: {inventory[key]}", end="")
-        if inventory[key] == 1:
-            print(" unit ", end="")
-        else:
-            print(" units ", end="")
-        print(f"({percentage}%)")
+        if inventory[key] > biggest:
+            biggest = inventory[key]
+        if inventory[key] < smallest:
+            smallest = inventory[key]
+    saved_biggest = biggest
+
+    while biggest > -1:
+        for key in inventory:
+            if inventory[key] == biggest:
+                if sorted.get(inventory[key], False) is False:
+                    temp = {key: inventory[key]}
+                    sorted.update({inventory[key]: temp})
+                else:
+                    sorted[inventory[key]].update({key: inventory[key]})
+            else:
+                if sorted.get(inventory[key], False) is False:
+                    temp = {key: inventory[key]}
+                    sorted.update({inventory[key]: temp})
+                else:
+                    sorted[inventory[key]].update({key: inventory[key]})
+        biggest -= 1
+
+    biggest = saved_biggest
+    while biggest > -1:
+        if sorted.get(biggest, False) is not False:
+            for key in sorted[biggest]:
+                percentage = (sorted[biggest][key] / values_total) * 100
+                float_percentage = "%.1f" % percentage
+                print(f"{key}: {sorted[biggest][key]}", end="")
+                if biggest == 1:
+                    print(" unit ", end="")
+                else:
+                    print(" units ", end="")
+                print(f"({float_percentage}%)")
+        biggest -= 1
 
     print("\n=== Inventory Statistics ===")
+    most_key: str = None
+    most_value: int = 0
+    for key in sorted[saved_biggest]:
+        if most_key is None:
+            most_key = key
+            most_value = sorted[saved_biggest][key]
+    print(f"Most abundant: {most_key} ({most_value} units)")
+    least_key: str = None
+    least_value: int = 0
+    for key in sorted[smallest]:
+        if least_key is None:
+            least_key = key
+            least_value = sorted[smallest][key]
+    print(f"Least abundant: {least_key} ({least_value} units)")
+
+    print("\n=== Inventory Categories ===")
+    nested: dict[dict: str[str, int]] = {}
+    for key in inventory:
+        if inventory[key] >= 4:
+            if nested.get("Moderate", False) is False:
+                moderate = {key: inventory[key]}
+                nested.update({"Moderate": moderate})
+            else:
+                nested["Moderate"].update({key: inventory[key]})
+        else:
+            if nested.get("Scarce", False) is False:
+                scarce = {key: inventory[key]}
+                nested.update({"Scarce": scarce})
+            else:
+                nested["Scarce"].update({key: inventory[key]})
+
+    print(f"{'Moderate'}: {nested['Moderate']}")
+    print(f"{'Scarce'}: {nested['Scarce']}")
+
+    print("\n=== Management Suggestions ===")
+    restocks = sorted.get(1)
+    restock_size = len(restocks)
+    print("Restock needed: ", end="")
+    for key in restocks:
+        print(key, end="")
+        if (restock_size > 1):
+            restock_size -= 1
+            print(", ", end="")
+        else:
+            print("")
+
+    print("\n=== Dictionary Properties Demo ===")
+    inv_dictionary: list[str: int] = inventory.items()
+    print("Dictionary keys: ", end="")
+    odd_even: int = 1
+    for items in inv_dictionary:
+        for key in items:
+            if odd_even == 1:
+                odd_even += 1
+                print(key, end="")
+                if (total_keys > 1):
+                    total_keys -= 1
+                    print(", ", end="")
+                else:
+                    print("")
+            else:
+                odd_even -= 1
+    total_values: int = len(inventory)
+
+    print("Dictionary values: ", end="")
+    odd_even = 1
+    for items in inv_dictionary:
+        for key in items:
+            if odd_even == 2:
+                odd_even -= 1
+                print(key, end="")
+                if (total_values > 1):
+                    total_values -= 1
+                    print(", ", end="")
+                else:
+                    print("")
+            else:
+                odd_even += 1
+
+    lookup: int = inventory.get("sword")
+    if lookup:
+        print("Sample lookup - 'sword' in inventory: True")
+    else:
+        print("Sample lookup - 'sword' in inventory: False")
 
 
 if __name__ == "__main__":
---
+++ Milestone 2/Python Modules/PM03/ex5/ft_data_stream.py
@@ -0,0 +1,125 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_data_stream.py                                 :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: joshde-s <joshde-s@student.42porto.com>   +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/02/25 13:31:21 by joshde-s        #+#    #+#               #
+#  Updated: 2026/02/25 18:16:43 by joshde-s        ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+from typing import Generator
+
+
+def fibonacci(repeats: int) -> Generator[int, None, None]:
+    """
+    This Fibonacci generator takes in a number and then does a for loop
+    within that range to repeatedly take a and add it to b to yield a sum
+    of the two while still remembering the result of the last call.
+    """
+    a: int = 0
+    b: int = 1
+    for n in range(repeats):
+        yield a
+        a, b = b, a + b
+
+def game_event(count: int) -> Generator[dict[str: int or str], None, None]:
+    """
+    A generator function which takes in a number of events that could be run.
+    The generator holds on to a group of players, their levels and current exp.
+    A player is picked from the modular of the current event count, and they
+    gain exp based on the modular of the event count.
+    Events are then determined by leveling up (passing 100 exp), or modular of
+    the event count to either be a treasure picked up or a monster encounter.
+    neither of the 3 can occur thus filtering out non important events.
+    """
+    alice: dict[str: int] = {"level": 7, "exp": 10}
+    bob: dict[str: int] = {"level": 5, "exp": 16}
+    charlie: dict[str: int] = {"level": 1, "exp": 0}
+    player_arr: list[str] = ["alice", "bob", "charlie"]
+    players: dict[str: dict] = {"alice": alice, "bob": bob, "charlie": charlie}
+    for i in range(1, count + 1):
+        player: str = player_arr[i % 3]
+        exp_gain: int = (i * 131) % 15
+        players[player]["exp"] += exp_gain
+        if players[player]["exp"] > 100:
+            players[player]["exp"] -= 100
+            players[player]["level"] += 1
+            level_up = True
+        else:
+            level_up = False
+        event: str = None
+        if level_up is True:
+            event = "leveled up"
+        elif i % 16 == 0:
+            event = "killed monster"
+        elif i % 4 == 0:
+            event = "found treasure"
+        yield {
+            "count": i,
+            "player": player,
+            "level": players[player]["level"],
+            "event": event
+        }
+
+
+def main() -> None:
+    """
+    A for loop will be used to call the 1000 game events. This will
+    automatically use the next() and iter() methods which would usuall be
+    called like this:
+    "ex.
+    event_generator = iter(game_event(1000))
+    while True:
+        try:
+            value = next(event_generator)
+            print(value)
+        except StopIteration:
+            break"
+    Analytics of the stream will be kept as the generator runs, then printed.
+    Then 2 generators will be shown, one to calculate fibonnacci and the other
+    to find a number of prime numbers
+    """
+    total_events: int = 0
+    total_treasures: int = 0
+    total_level_ups: int = 0
+    total_players: dict[str: int] = {}
+    print("Processing 1000 game events...\n")
+    for event in game_event(1000):
+        total_events += 1
+        if event["event"] is not None:
+            print(f"Event {event['count']}: Player {event['player']}",
+                  f"(level {event['level']}) {event['event']}")
+            if event["event"] == "found treasure":
+                total_treasures += 1
+            if event["event"] == "leveled up":
+                total_level_ups += 1
+                total_players[event["player"]] = event['level']
+
+    print("\n=== Stream Analytics ===\n")
+    print(f"Total events processed: {total_events}")
+    print(f"High-level players (lvl 10+): {len(total_players)}")
+    print(f"Treasure events: {total_treasures}")
+    print(f"Level-up events: {total_level_ups}\n")
+
+    print("Memory usage: Constant (streaming)\nProcessing time: 0.045 seconds")
+
+    print("\n=== Generator Demonstration ===")
+    times_to_repreat: int = 10
+    times_ran: int = 0
+    print("Fibonacci sequence (first 10): ", end="")
+    for num in fibonacci(times_to_repreat):
+        times_ran += 1
+        if times_ran < times_to_repreat:
+            print(f"{num}, ", end="")
+        else:
+            print(f"{num}")
+
+    times_to_repreat: int = 5
+    times_ran: int = 0
+    print("Prime numbers (first 5): ")
+    
+
+if __name__ == "__main__":
+    main()
