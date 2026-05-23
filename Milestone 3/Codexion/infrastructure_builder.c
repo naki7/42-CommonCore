b46e1323df145037f1174b97083c4a18a76f150b
Naki Neves <joshie.cad@gmail.com>
2026-05-23 18:44:13 +0100

    Made a bunch of changes as I learnt how to use pthread and mutex as well. Now with a better grasp on how they both function i simpley need to incorporate my typedef structures to then produce the right number of coders/dongles and then make the simulated environment functional.

--- Milestone 3/Codexion/infrastructure_builder.c
+++ Milestone 3/Codexion/infrastructure_builder.c
@@ -6,7 +6,25 @@
 /*   By: joshde-s <joshde-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 12:23:56 by joshde-s          #+#    #+#             */
-/*   Updated: 2026/05/22 12:23:57 by joshde-s         ###   ########.fr       */
+/*   Updated: 2026/05/23 18:35:17 by joshde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "libcodexion.h"
+
+t_coder		*assign_coders(int coder_num, int burnout_time);
+
+t_dongle	*assign_dongles(int cooldown);
+
+void	*base_build(int *configs, char *priority)
+{
+	t_coder			*coders;
+	t_dongle		*dongles;
+	t_monitor		*monitor;
+	pthread_mutex_t	print_lock;
+
+	pthread_mutex_init(&print_lock, NULL);
+	coders = assign_coders(configs[0], configs[1]);
+	dongles = assign_dongles(configs[6]);
+	monitor = (t_monitor *)(1, configs[0], configs[1], configs[2], configs[3], configs[4], coders, dongles);
+}
--- Milestone 3/Codexion/libcodexion.h
+++ Milestone 3/Codexion/libcodexion.h
@@ -6,7 +6,7 @@
 /*   By: joshde-s <joshde-s@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 13:49:29 by joshde-s          #+#    #+#             */
-/*   Updated: 2026/05/23 10:37:55 by joshde-s         ###   ########.fr       */
+/*   Updated: 2026/05/23 18:27:59 by joshde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,40 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <pthread.h>
 
-typedef struct s_stack
+typedef struct s_coder
 {
-	int				value;
-	struct s_stack	*next;
-}	t_stack;
+	int				n;
+	int				remaining_time;
+	pthread_t		thread;
+	struct s_dongle	*left;
+	struct s_dongle	*right;
+}	t_coder;
 
-int	*parser(int argc, char *argv[], int *arguments);
+typedef struct s_dongle
+{
+	int				usable;
+	int				cooldown;
+	pthread_mutex_t	lock;
+	pthread_cond_t	condition;
+}	t_dongle;
+
+typedef struct s_monitor
+{
+	int				state;
+	int				number_of_coders;
+	int				time_to_burnout;
+	int				time_to_compile;
+	int				time_to_debug;
+	int				time_to_refactor;
+	pthread_mutex_t	print_lock;
+	t_coder			*coders;
+	t_dongle		*dongles;
+}	t_monitor;
+
+int		*parser(int argc, char *argv[], int *arguments);
+
+void	*base_build(int *configs, char *priority);
 
 #endif
